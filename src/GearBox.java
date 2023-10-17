b44ac211565c07a43526a1d205c45ac57700478a
Behrozbek Togayev <teramotl@icloud.com>
2023-10-17 20:15:11 +0500

    inkapsulyacia, kak vizivat, ne interesno kak, glavnoe mojno vizvat

---
+++ src/GearBox.java
@@ -0,0 +1,25 @@
+public class GearBox {
+    /**
+     * 0 - neutral
+     * -1 - szadniy
+     * 1-6 skorost
+     */
+    private int maxGear = 6;
+    private int minGear = 1;
+    private int gear = 0;
+    public void shiftUp() {
+        gear = gear < maxGear ? gear +1 : gear;
+    }
+    public void shiftDown() {
+        gear = gear > minGear ? gear -1 : gear;
+    }
+    public void switchGear() {
+        gear = (gear > 1) ? gear : -1;
+    }
+    public void switchNeutral() {
+        gear = 0;
+    }
+    public int getCurrentGear() {
+        return gear;
+    }
+}
