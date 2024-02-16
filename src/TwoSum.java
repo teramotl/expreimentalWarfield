11ca1982a240453556926aec4caa2850cd1fe97c
Behrozbek Togayev <teramotl@icloud.com>
2024-02-16 22:19:37 +0500

    twosum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -2,12 +2,13 @@ import java.util.HashMap;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-
-        for(int i = 0; i < nums.length; i++) {
-
-            int subtstracted = target - nums[i];
-            hashMap.put(subtstracted, i);
-            if ()
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
         }
+        return null;
+    }
 }
